6a1e018d3e3f408749298e1825a4caf3b6e6c332
Balu Murugesan <balu.m@testyantra.in>
2023-08-01 11:52:54 +0530

    updATED

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { Link, RouterProvider, createBrowserRouter } from "react-router-dom";
+import DeepLinking from "./deeplinking";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const router = createBrowserRouter([
+    {
+      path: "/deeplinking",
+      element: (
+        <div>
+          <DeepLinking />
+        </div>
+      ),
+    },
+    {
+      path: "/",
+      element: (
+        <div>
+          <Link to="/deeplinking">click to check deep linking</Link>
+        </div>
+      ),
+    },
+  ]);
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/deeplinking.js
@@ -0,0 +1,4 @@
+const DeepLinking = () => {
+  return "HI";
+};
+export default DeepLinking;
